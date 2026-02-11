41dee7952a7f49279ba4a30c7f6a97ba5da3300f
nadhem <nadhem.saidani@esprit.tn>
2026-02-11 18:58:48 +0100

    Ajout de mes modifications

---
+++ integration/SmartResearchPublicationManagement_V001/build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/debug/moc_smartresearchpublicationmanagement.cpp
@@ -0,0 +1,451 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'smartresearchpublicationmanagement.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.7.3)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../../smartresearchpublicationmanagement.h"
+#include <QtCore/qmetatype.h>
+
+#include <QtCore/qtmochelpers.h>
+
+#include <memory>
+
+
+#include <QtCore/qxptype_traits.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'smartresearchpublicationmanagement.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.7.3. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+QT_WARNING_DISABLE_GCC("-Wuseless-cast")
+namespace {
+
+#ifdef QT_MOC_HAS_STRINGDATA
+struct qt_meta_stringdata_CLASSSmartResearchPublicationManagementENDCLASS_t {};
+constexpr auto qt_meta_stringdata_CLASSSmartResearchPublicationManagementENDCLASS = QtMocHelpers::stringData(
+    "SmartResearchPublicationManagement",
+    "on_btnLoginSubmit_clicked",
+    "",
+    "on_BtnTheme_clicked",
+    "on_BtnEmployee_clicked",
+    "on_BtnFinance_clicked",
+    "on_BtnPublication_clicked",
+    "on_BtnLabs_clicked",
+    "on_BtnInventory_clicked",
+    "on_BtnPlans_clicked",
+    "on_BtnAdd_clicked",
+    "on_BtnEdit_clicked",
+    "on_BtnPopupCancelFinance_clicked",
+    "on_BtnPopupSaveFinance_clicked",
+    "on_BtnEmployee_2_clicked",
+    "on_ADD_user_2_clicked",
+    "on_pushButton_13_clicked",
+    "on_pushButton_14_clicked",
+    "on_pushButton_15_clicked",
+    "on_pushButton_34_clicked",
+    "on_pushButton_35_clicked",
+    "on_pushButton_36_clicked",
+    "on_pushButton_37_clicked",
+    "on_pushButton_39_clicked",
+    "on_pushButton_40_clicked",
+    "on_Cancel_2_clicked",
+    "on_Update_R_2_clicked",
+    "on_btnCancelUpdate_clicked",
+    "on_btnLogin_clicked",
+    "on_BtnLogout_clicked",
+    "on_pushButton_clicked",
+    "on_Quitter_clicked",
+    "on_btnAjouter_clicked",
+    "on_btnRetour_2_clicked",
+    "on_btnAjouter_5_clicked",
+    "on_btnmodifier_clicked",
+    "on_btnRetour_clicked",
+    "on_btnAjouter_2_clicked",
+    "on_BtnLabAdd_2_clicked",
+    "on_BtnLabEdit_2_clicked",
+    "on_pushButton_2_clicked",
+    "on_annuleL_clicked",
+    "on_pushButton_5_clicked",
+    "on_BtnInventoryAdd_clicked",
+    "on_BtnInventoryEdit_clicked",
+    "on_BtnPopupCancelInventory_3_triggered",
+    "QAction*",
+    "arg1",
+    "on_BtnPopupCancelInventory_2_triggered",
+    "on_BtnPopupCancelInventory_3_clicked",
+    "on_BtnPopupCancelInventory_2_clicked",
+    "on_BtnPopupCancelInventory_4_clicked",
+    "on_BtnPopupCancelInventory_5_clicked",
+    "on_btnAjouter_3_clicked",
+    "on_btnmodifier_2_clicked",
+    "on_btnRetour_5_clicked",
+    "on_btnRetour_6_clicked"
+);
+#else  // !QT_MOC_HAS_STRINGDATA
+#error "qtmochelpers.h not found or too old."
+#endif // !QT_MOC_HAS_STRINGDATA
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_CLASSSmartResearchPublicationManagementENDCLASS[] = {
+
+ // content:
+      12,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      53,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,  332,    2, 0x08,    1 /* Private */,
+       3,    0,  333,    2, 0x08,    2 /* Private */,
+       4,    0,  334,    2, 0x08,    3 /* Private */,
+       5,    0,  335,    2, 0x08,    4 /* Private */,
+       6,    0,  336,    2, 0x08,    5 /* Private */,
+       7,    0,  337,    2, 0x08,    6 /* Private */,
+       8,    0,  338,    2, 0x08,    7 /* Private */,
+       9,    0,  339,    2, 0x08,    8 /* Private */,
+      10,    0,  340,    2, 0x08,    9 /* Private */,
+      11,    0,  341,    2, 0x08,   10 /* Private */,
+      12,    0,  342,    2, 0x08,   11 /* Private */,
+      13,    0,  343,    2, 0x08,   12 /* Private */,
+      14,    0,  344,    2, 0x08,   13 /* Private */,
+      15,    0,  345,    2, 0x08,   14 /* Private */,
+      16,    0,  346,    2, 0x08,   15 /* Private */,
+      17,    0,  347,    2, 0x08,   16 /* Private */,
+      18,    0,  348,    2, 0x08,   17 /* Private */,
+      19,    0,  349,    2, 0x08,   18 /* Private */,
+      20,    0,  350,    2, 0x08,   19 /* Private */,
+      21,    0,  351,    2, 0x08,   20 /* Private */,
+      22,    0,  352,    2, 0x08,   21 /* Private */,
+      23,    0,  353,    2, 0x08,   22 /* Private */,
+      24,    0,  354,    2, 0x08,   23 /* Private */,
+      25,    0,  355,    2, 0x08,   24 /* Private */,
+      26,    0,  356,    2, 0x08,   25 /* Private */,
+      27,    0,  357,    2, 0x08,   26 /* Private */,
+      28,    0,  358,    2, 0x08,   27 /* Private */,
+      29,    0,  359,    2, 0x08,   28 /* Private */,
+      30,    0,  360,    2, 0x08,   29 /* Private */,
+      31,    0,  361,    2, 0x08,   30 /* Private */,
+      32,    0,  362,    2, 0x08,   31 /* Private */,
+      33,    0,  363,    2, 0x08,   32 /* Private */,
+      34,    0,  364,    2, 0x08,   33 /* Private */,
+      35,    0,  365,    2, 0x08,   34 /* Private */,
+      36,    0,  366,    2, 0x08,   35 /* Private */,
+      37,    0,  367,    2, 0x08,   36 /* Private */,
+      38,    0,  368,    2, 0x08,   37 /* Private */,
+      39,    0,  369,    2, 0x08,   38 /* Private */,
+      40,    0,  370,    2, 0x08,   39 /* Private */,
+      41,    0,  371,    2, 0x08,   40 /* Private */,
+      42,    0,  372,    2, 0x08,   41 /* Private */,
+      43,    0,  373,    2, 0x08,   42 /* Private */,
+      44,    0,  374,    2, 0x08,   43 /* Private */,
+      45,    1,  375,    2, 0x08,   44 /* Private */,
+      48,    1,  378,    2, 0x08,   46 /* Private */,
+      49,    0,  381,    2, 0x08,   48 /* Private */,
+      50,    0,  382,    2, 0x08,   49 /* Private */,
+      51,    0,  383,    2, 0x08,   50 /* Private */,
+      52,    0,  384,    2, 0x08,   51 /* Private */,
+      53,    0,  385,    2, 0x08,   52 /* Private */,
+      54,    0,  386,    2, 0x08,   53 /* Private */,
+      55,    0,  387,    2, 0x08,   54 /* Private */,
+      56,    0,  388,    2, 0x08,   55 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 46,   47,
+    QMetaType::Void, 0x80000000 | 46,   47,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject SmartResearchPublicationManagement::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_CLASSSmartResearchPublicationManagementENDCLASS.offsetsAndSizes,
+    qt_meta_data_CLASSSmartResearchPublicationManagementENDCLASS,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSSmartResearchPublicationManagementENDCLASS_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<SmartResearchPublicationManagement, std::true_type>,
+        // method 'on_btnLoginSubmit_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_BtnTheme_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_BtnEmployee_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_BtnFinance_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_BtnPublication_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_BtnLabs_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_BtnInventory_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_BtnPlans_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_BtnAdd_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_BtnEdit_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_BtnPopupCancelFinance_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_BtnPopupSaveFinance_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_BtnEmployee_2_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_ADD_user_2_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_13_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_14_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_15_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_34_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_35_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_36_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_37_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_39_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_40_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_Cancel_2_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_Update_R_2_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnCancelUpdate_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnLogin_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_BtnLogout_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_Quitter_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnAjouter_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnRetour_2_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnAjouter_5_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnmodifier_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnRetour_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnAjouter_2_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_BtnLabAdd_2_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_BtnLabEdit_2_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_2_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_annuleL_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_5_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_BtnInventoryAdd_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_BtnInventoryEdit_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_BtnPopupCancelInventory_3_triggered'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<QAction *, std::false_type>,
+        // method 'on_BtnPopupCancelInventory_2_triggered'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<QAction *, std::false_type>,
+        // method 'on_BtnPopupCancelInventory_3_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_BtnPopupCancelInventory_2_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_BtnPopupCancelInventory_4_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_BtnPopupCancelInventory_5_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnAjouter_3_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnmodifier_2_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnRetour_5_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnRetour_6_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
+    >,
+    nullptr
+} };
+
+void SmartResearchPublicationManagement::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<SmartResearchPublicationManagement *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 1: _t->on_BtnTheme_clicked(); break;
+        case 2: _t->on_BtnEmployee_clicked(); break;
+        case 3: _t->on_BtnFinance_clicked(); break;
+        case 4: _t->on_BtnPublication_clicked(); break;
+        case 5: _t->on_BtnLabs_clicked(); break;
+        case 6: _t->on_BtnInventory_clicked(); break;
+        case 7: _t->on_BtnPlans_clicked(); break;
+        case 8: _t->on_BtnAdd_clicked(); break;
+        case 9: _t->on_BtnEdit_clicked(); break;
+        case 10: _t->on_BtnPopupCancelFinance_clicked(); break;
+        case 11: _t->on_BtnPopupSaveFinance_clicked(); break;
+        case 12: _t->on_BtnEmployee_2_clicked(); break;
+        case 13: _t->on_ADD_user_2_clicked(); break;
+        case 14: _t->on_pushButton_13_clicked(); break;
+        case 15: _t->on_pushButton_14_clicked(); break;
+        case 16: _t->on_pushButton_15_clicked(); break;
+        case 17: _t->on_pushButton_34_clicked(); break;
+        case 18: _t->on_pushButton_35_clicked(); break;
+        case 21: _t->on_pushButton_39_clicked(); break;
+        case 22: _t->on_pushButton_40_clicked(); break;
+        case 23: _t->on_Cancel_2_clicked(); break;
+        case 24: _t->on_Update_R_2_clicked(); break;
+        case 25: _t->on_btnCancelUpdate_clicked(); break;
+        case 27: _t->on_BtnLogout_clicked(); break;
+        case 29: _t->on_Quitter_clicked(); break;
+        case 30: _t->on_btnAjouter_clicked(); break;
+        case 31: _t->on_btnRetour_2_clicked(); break;
+        case 32: _t->on_btnAjouter_5_clicked(); break;
+        case 33: _t->on_btnmodifier_clicked(); break;
+        case 34: _t->on_btnRetour_clicked(); break;
+        case 35: _t->on_btnAjouter_2_clicked(); break;
+        case 36: _t->on_BtnLabAdd_2_clicked(); break;
+        case 37: _t->on_BtnLabEdit_2_clicked(); break;
+        case 39: _t->on_annuleL_clicked(); break;
+        case 40: _t->on_pushButton_5_clicked(); break;
+        case 41: _t->on_BtnInventoryAdd_clicked(); break;
+        case 42: _t->on_BtnInventoryEdit_clicked(); break;
+        case 45: _t->on_BtnPopupCancelInventory_3_clicked(); break;
+        case 46: _t->on_BtnPopupCancelInventory_2_clicked(); break;
+        case 47: _t->on_BtnPopupCancelInventory_4_clicked(); break;
+        case 48: _t->on_BtnPopupCancelInventory_5_clicked(); break;
+        case 49: _t->on_btnAjouter_3_clicked(); break;
+        case 50: _t->on_btnmodifier_2_clicked(); break;
+        case 51: _t->on_btnRetour_5_clicked(); break;
+        case 52: _t->on_btnRetour_6_clicked(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType(); break;
+        case 43:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType(); break;
+            case 0:
+                *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType::fromType< QAction* >(); break;
+            }
+            break;
+        case 44:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType(); break;
+            case 0:
+                *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType::fromType< QAction* >(); break;
+            }
+            break;
+        }
+    }
+}
+
+const QMetaObject *SmartResearchPublicationManagement::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *SmartResearchPublicationManagement::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_CLASSSmartResearchPublicationManagementENDCLASS.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int SmartResearchPublicationManagement::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 53)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 53;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 53)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 53;
+    }
+    return _id;
+}
+QT_WARNING_POP
