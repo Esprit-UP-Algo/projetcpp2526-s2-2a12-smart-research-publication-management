849cae2d1a8c287650c85924c1d26012c093feca
rayen123-ui <rrayyyrrayyy@gmail.com>
2026-03-27 01:43:51 +0100

    l'ajout des metiers de publications et les metiers avancees de publication

---
+++ SmartResearchPublicationManagement_V003/build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_SmartResearchPublicationManagement_FILE C:/Users/MSI/Desktop/mon projet c++/SmartResearchPublicationManagement_V003/build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/SmartResearchPublicationManagement.exe)
+set(__QT_DEPLOY_TARGET_SmartResearchPublicationManagement_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_SmartResearchPublicationManagement_RUNTIME_DLLS C:/Qt/6.7.3/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Sql.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Core.dll)
